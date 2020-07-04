636b7f26ecddd065f5d3fd3a89c8293d1e9ee24d
therealbutterfly <67443161+therealbutterfly@users.noreply.github.com>
2020-07-04 17:14:58 -0400

    Decision TRee and Metrics

--- Filtering.py
+++ Filtering.py
@@ -1,115 +1,67 @@
-#Preparation by importing libraries
-
+#importing libraries
 import pandas as pd
 import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
-from scipy.stats import shapiro
-import matplotlib.patches as mpatches
 
-#Defining plot style
-plt.style.use('ggplot')
-pd.set_option('display.max_columns', 500)
-color_dict = dict({0:'green',
-            1:'red'})
+#Decision Tree Import
+from sklearn.tree import DecisionTreeClassifier
 
-#Importing data as dataframe
+# Import train_test_split function
+from sklearn.model_selection import TimeSeriesSplit
 
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+
+#Importing data
 filename = r"creditcard.csv"
 df = pd.read_csv(filename)
-
-#Check to make sure data is imported correctly
-print(df.head())
-
-#Checking data types
-print(df.dtypes)
-
-#Changing Class datatype from int64 to Category
-
 for col in ['Class']:
     df[col] = df[col].astype('category')
 
-#Generating a random combinations of scatterplots to check for data balance
-# and class overlap
-sn.scatterplot(x="V18",y="V26", hue="Class", data=df, palette=color_dict)
-plt.show()
-sn.scatterplot(x="V1",y="V19", hue="Class", data=df, palette=color_dict)
-plt.show()
-sn.scatterplot(x="V11",y="V25", hue="Class", data=df, palette=color_dict)
-plt.show()
-sn.scatterplot(x="V4",y="V12", hue="Class", data=df, palette=color_dict)
-plt.show()
-sn.scatterplot(x="V21",y="V14", hue="Class", data=df, palette=color_dict)
-plt.show()
-sn.scatterplot(x="V15",y="V25", hue="Class", data=df, palette=color_dict)
-plt.show()
+#splitting into features and class
+X = df.loc[:, 'Time':'Amount']
+y = df.loc[:, 'Class']
 
-# Some scatterplots show that high class overlap, whereas some show
-#more delineation between classes
-# This indicates that a feature subset can be defined such that
-#the model performs better than with the full dataset
+# Create Decision Tree classifer object
+clf = DecisionTreeClassifier()
 
-#Counting number of NaN values in dataframe
-print(df.isnull().sum().sum())
+#KFold Cross (with time series split) Validation approach
+tss = TimeSeriesSplit(n_splits = 3)
+tss.split(X)
 
-#Basic Description of datasets
+# Initialize the accuracy of the models to blank list. The accuracy of each model will be appended to this list
+balanced_accuracy_model = []
+average_precision_model = []
+f1_score_model = []
+roc_auc_model = []
 
-print(df.describe())
+# Iterate over each train-test split
+for train_index, test_index in tss.split(X):
+    # Split train-test
+    X_train, X_test = X.iloc[train_index, :], X.iloc[test_index,:]
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+    # Train the model
+    model = clf.fit(X_train, y_train)
+    # Append metrics to the list
+    balanced_accuracy_model.append(balanced_accuracy_score(y_test, model.predict(X_test)))
+    average_precision_model.append(average_precision_score(y_test, model.predict(X_test)))
+    f1_score_model.append(f1_score(y_test, model.predict(X_test)))
+    roc_auc_model.append(roc_auc_score(y_test, model.predict(X_test)))
 
-# Count of each feature is the same, confirming that there are no missing values
-# Max time is 1720,792 confirming that the data covers just above 2 days from
-#first transaction recorded
-# 50th percentile is 84,692 which indicates that data is generally evenly
-#distributed across two days
-# Maximum amount is 25,691 and the minimum transaction amount is $0, and the
-#average amount is $88
-# $77 is the 75th percentile which indicates that the amount field is positively
-#skewed, with a few high value outliers
-# The average class is 0.001727which confirms that the dataset is heavily imbalanced.
+# Print the model metrics
+metrics = pd.DataFrame(
+    {'Trial': ["Trial1", "Trial2", "Trial3"],
+    'Balanced Accurace': balanced_accuracy_model,
+    'Average Precision': average_precision_model,
+    'F1 Score': f1_score_model,
+    'ROC_AUC': roc_auc_model
+    })
 
-#Exploring data distribution
-df.hist()
-plt.show()
+print(metrics)
 
-#At first glance, all  fields seem to be unimodal, with different means
-
-#Further exploring the distribution of 'Time' field
-df.hist(column='Time',by='Class')
-plt.show()
-
-# The drilled-down distribution shows a bi-modal distribution shows that
-#there are fewer transacations at night
-#There is a more distinct bi-modality for non-fraudulent transacations which
-#indicates cycles to regular transacations that fraudulent transactions may
-#not follow
-
-#Further exploring the distribution of 'Amount' field
-
-df.hist(column='Amount',by="Class")
-plt.show()
-
-# Amount looks to have a high positive skew, with a few extreme values,
-#as indicated by the percentile distribution
-
-#Futher examining the distribution of Amount by Class without points beyond
-#the whiskers (outliers)
-df.boxplot(column='Amount',by="Class",showfliers=False)
-plt.show()
-
-# Creating a scatter matrix
-scatter_matrix(df)
-plt.show()
-
-#Creating correlation matrix rounding to 4 digits
-corr = df.corr().round(4)
-
-#Printing to textfile
-print(corr, file=open("corr_output.txt","w"))
-
-################### Need to close file!
-
-#Visualizing correlation matrix
-sn.heatmap(corr,annot=False)
-
-plt.show()
+#Feature Selection using filtering 1 - SelecFromModel
