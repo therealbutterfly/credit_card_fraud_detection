74a8c1f8eee350ee68268a980193044d4c1d1f7a
therealbutterfly <67443161+therealbutterfly@users.noreply.github.com>
2020-07-24 17:45:13 -0400

    Update measures

--- Feature Selection.py
+++ Feature Selection.py
@@ -5,7 +5,7 @@ import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import TimeSeriesSplit, cross_val_score
+from sklearn.model_selection import TimeSeriesSplit, cross_val_score, cross_validate
 from sklearn import metrics
 from sklearn.metrics import fbeta_score
 from sklearn.metrics import recall_score, roc_auc_score, brier_score_loss, confusion_matrix
@@ -18,7 +18,6 @@ from numpy import mean, std
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import  mutual_info_classif
 
-
 #Importing data
 filename = r"creditcard.csv"
 df = pd.read_csv(filename)
@@ -29,6 +28,8 @@ for col in ['Class']:
 X = df.loc[:, 'Time':'Amount']
 y = df.loc[:, 'Class']
 
+
+'''
 #############################################################################
 #DECISION TREE WITHOUT FILTERING
 
@@ -64,7 +65,7 @@ metrics = pd.DataFrame(
     {'Result': ["Average"],
     'F_measure': np.average(F_measure_model),
     'ROC_AUC': np.average(roc_auc_model),
-    'Brier_Score' : np.average(brier_score_model),
+    'Brier_Score' : np.average(brier_score_model)
     })
 print("Model Metrics:")
 print(metrics)
@@ -75,7 +76,7 @@ confusion = pd.DataFrame(
     'Confusion_Matrix' : confusion_matrix_model
     })
 print(confusion)
-
+'''
 '''
 ###############################################################################
 #APPLYING RFE FEATURE SELECTION METHOD TO IDENTIFY OPTIMAL NUMBER OF COLUMNS
@@ -121,22 +122,35 @@ for i in range(X.shape[1]):
 '''
 '''
 ##############################################################################
-#APPLYING SELECTKBEST FEATURES WITH MUTUAL INFORMATION
+#APPLYING SELECTKBEST FEATURES WITH F_SCORE
 
-# Import the necessary libraries first
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import  mutual_info_classif
+# Create Decision Tree classifer object
+clf = DecisionTreeClassifier()
 
-for i in range(10,25):
-    sel_mutual = SelectKBest(mutual_info_classif, k=i)
-    X_train_mutual = sel_mutual.fit_transform(X,y)
-    print(sel_mutual.get_support())
+#KFold Cross (with time series split) Validation approach
+tss = TimeSeriesSplit(n_splits = 2)
+tss.split(X)
 
-###############################################################################
-# APPLYING DECISION TREE ALL COMBINATIONS ABOVE TO FIND OPTIMAL NUMBER OF FEATURES
+#Select K Best and run model
+for i in range(1,30):
+    selector = SelectKBest(k=i)
+    X_new = pd.DataFrame(selector.fit_transform(X, y))
+    #print columns
+    cols = selector.get_support(indices=True)
+    print("Columns Identified:", cols)
+    # Iterate over each train-test split
+    for train_index, test_index in tss.split(X):
+        # Split train-test
+        X_train, X_test = X_new.iloc[train_index, :], X_new.iloc[test_index,:]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        # Train the model
+        model = clf.fit(X_train, y_train)
+        # Append metrics to the list
+        print("fbeta:", fbeta_score(y_test, model.predict(X_test),beta=2))
+        print("AUC:", roc_auc_score(y_test, model.predict(X_test)))
+        print("Brier:", brier_score_loss(y_test, model.predict(X_test)))
+        print("Confusion_matrix:", confusion_matrix(y_test, model.predict(X_test)))
 
-
-'''
 '''
 ##############################################################################
 #STEP FORWARD FEATURE MODEL SELECTION
@@ -145,52 +159,51 @@ import mlxtend as mlx
 from mlxtend.feature_selection import SequentialFeatureSelector as sfs
 
 #KFold Cross (with time series split) Validation approach
-tss = TimeSeriesSplit(n_splits = 3)
+tss = TimeSeriesSplit(n_splits = 2)
 tss.split(X)
 
 # Create Decision Tree classifer object
 clf = DecisionTreeClassifier()
 
 #make balanced scorer
-scorer = make_scorer(balanced_accuracy_score)
+scorer = make_scorer(fbeta_score, beta=2)
+
+# Build step forward feature selection
+sfs1 = sfs(clf,
+        k_features=i,
+        forward=True,
+        floating=False,
+        verbose=2,
+        scoring=scorer)
 
 # Iterate over each train-test split
 for train_index, test_index in tss.split(X):
     # Split train-test
     X_train, X_test = X.iloc[train_index, :], X.iloc[test_index,:]
     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+    # Perform SFFS
+    for i in range(1,30)
+    sfs1 = sfs1.fit(X_train, y_train)
+    # Which features?
+    print(list(sfs1.k_feature_idx_))
 
-# Build step forward feature selection
-sfs1 = sfs(clf,
-        k_features=24,
-        forward=True,
-        floating=False,
-        verbose=2,
-        scoring=scorer)
-# Perform SFFS
-sfs1 = sfs1.fit(X_train, y_train)
 
-# Which features?
-feat_cols = list(sfs1.k_feature_idx_)
-print(feat_cols)
-
-'''
-'''
 #############################################################################
-#FINDING THE OPTIMAL NUMBER OF COLUMNS AUTOMATICALLY WITH RFECV (error)
-
+#FINDING THE OPTIMAL NUMBER OF COLUMNS AUTOMATICALLY WITH RFECV
+'''
 # create pipeline
 rfecv_model = RFECV(estimator=DecisionTreeClassifier())
 model = DecisionTreeClassifier()
 pipeline = Pipeline(steps=[('s',rfecv_model),('m',model)])
 
 #make balanced scorer
-scorer = make_scorer(balanced_accuracy_score)
+scorer = make_scorer(fbeta_score, beta=2)
 
 # evaluate model
 cv = TimeSeriesSplit(n_splits=3)
 result = cross_validate(pipeline, X, y, scoring=scorer,
                           cv=cv, return_estimator=True)
+print(result)
 
 #feature selected for each iteration
 for iter, pipe in enumerate(result['estimator']):
@@ -199,8 +212,53 @@ for iter, pipe in enumerate(result['estimator']):
         print('Column: %d, Selected %s, Rank: %d' %
             (i, pipe['s'].support_[i], pipe['s'].ranking_[i]))
 
-#Identify which columns were used
-print(X.shap
-for i in range(X.shape[1]):
-	print('Column: %d, Selected %s, Rank: %.3f' % (i, rfecv_model.support_[i], rfecv_model.ranking_[i]))
+### RUNNING DECISION TREE WITH OPTIMAL RFECV COLUMNS
+
+#splitting into features and class
+X = df.loc[:, ['V10','V12','V14','V17']]
+y = df.loc[:, 'Class']
+
+# Create Decision Tree classifer object
+clf = DecisionTreeClassifier()
+
+#KFold Cross (with time series split) Validation approach
+tss = TimeSeriesSplit(n_splits = 3)
+tss.split(X)
+
+# Initialize the accuracy of the models to blank list. The accuracy of each model will be appended to this list
+F_measure_model = []
+roc_auc_model = []
+brier_score_model = []
+confusion_matrix_model = []
+
+# Iterate over each train-test split
+for train_index, test_index in tss.split(X):
+    # Split train-test
+    X_train, X_test = X.iloc[train_index, :], X.iloc[test_index,:]
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+    # Train the model
+    model = clf.fit(X_train, y_train)
+    # Append metrics to the list
+    F_measure_model.append(fbeta_score(y_test, model.predict(X_test),beta=2))
+    roc_auc_model.append(roc_auc_score(y_test, model.predict(X_test)))
+    brier_score_model.append(brier_score_loss(y_test, model.predict(X_test)))
+    confusion_matrix_model.append(confusion_matrix(y_test, model.predict(X_test)))
+
+# Print the model metrics
+metrics = pd.DataFrame(
+    {'Result': ["Average"],
+    'F_measure': np.average(F_measure_model),
+    'ROC_AUC': np.average(roc_auc_model),
+    'Brier_Score' : np.average(brier_score_model)
+    })
+
+print("Model Metrics:")
+print(metrics)
+
+print("Confusion Matrix")
+confusion = pd.DataFrame(
+    {'Trial': ["Trial 1", "Trial 2", "Trial 3"],
+    'Confusion_Matrix' : confusion_matrix_model
+    })
+print(confusion)
 '''
